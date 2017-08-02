496e53a3a4f685bfa052681e9218d1e257947df4
YunsChou <2647754496@qq.com>
2017-08-02 09:14:08 +0800

    Initial commit

---
+++ Example/Mediator/LpViewController.h
@@ -0,0 +1,13 @@
+//
+//  LpViewController.h
+//  Mediator
+//
+//  Created by YunsChou on 08/02/2017.
+//  Copyright (c) 2017 YunsChou. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LpViewController : UIViewController
+
+@end
